1dd0eb156b6547d32f1cc50fa33eb19439a7d950
pavlo <pavlo_ivanov@yahoo.com>
2022-10-30 22:13:32 +0100

    Step 18 - Create REST API - create Views and ViewSets in views.py_5.17.

--- api/coronavstech/companies/serializers.py
+++ api/coronavstech/companies/serializers.py
@@ -4,5 +4,12 @@ from .models import Company
 
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
-        model = Company
-        fields = ["id", "name", "status", "application_link", "last_update", "notes"]
+        model = Company  # (1)
+        fields = ["id", "name", "status", "application_link", "last_update", "notes"]  # (2)
+
+
+"""
+Notes:
+(1) specify which model you wan to serialize. 
+(2) this is what we want to serialize.
+"""
--- api/coronavstech/companies/views.py
+++ api/coronavstech/companies/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Company
+from serializers import CompanySerializer
+
+
+class CompanyViewSet(ModelViewSet):
+    serializer_class = CompanySerializer  # (1)
+    queryset = Company.objects.all().order_by("-last_update")  # (2)
+    pagination_class = PageNumberPagination  # (3)
+
+
+"""
+Notes:
+(1) here we write the serializer class name from serializers.py.
+(2) queryset is what do we want to return. We want to return all the 
+companies. Companies.objects.all() is Django Object Relational Mapper(ORM).
+It is a way to talk to SQLite3 database instead of making SQL query. 
+It is an object oriented obstruction of our database. 
+
+Companies.objects.all() = SELECT * from Company
+
+Then we order them by last update.
+
+(3) we create pages numbers so that our Server is working fast. 
+This is done so that we don't get timedout as only part of the pages are received at a time. 
+"""
