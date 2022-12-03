7c59a1f0bcfb438174a51b0d1ddfcea6f8fa10cc
pavlo <pavlo_ivanov@yahoo.com>
2022-12-04 00:20:15 +0100

    Step 24 - API test Class (unittest style). Cleaning the code by using setUp and tearDown. Look at explanation in notebook 'Pytest automatic testing for our Django application_Section 6' in 'Step 24 - API test Class (unittest style). Cleaning the code by using setUp and tearDown'

--- api/coronavstech/companies/tests/test_api.py
+++ api/coronavstech/companies/tests/test_api.py
@@ -8,17 +8,23 @@ from api.coronavstech.companies.models import Company
 
 """
 Look at explanation in notebook 'Pytest automatic testing for our Django application_Section 6'
-in 'Step 23 - Writing our first pytests'
+in 'Step 24 - API test Class (unittest style). Cleaning the code by using setUp and tearDown'
 """
+
 
 @pytest.mark.django_db
 class TestGetCompanies(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.companies_url = reverse("companies-list")
+
+    def tearDown(self) -> None:
+        pass
+
     def test_zero_companies_should_return_empty_list(self) -> None:
         """Test that we GET zero companies. If we fetch the GET request we expect to get empty list."""
-        client = Client()
         # companies_url = "http://127.0.0.1:8000/companies/"
-        companies_url = reverse("companies-list")
-        response = client.get(companies_url)
+        response = self.client.get(self.companies_url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(json.loads(response.content), [])
 
@@ -26,10 +32,8 @@ class TestGetCompanies(TestCase):
         """We want to have one company in our test database and
         when we retrieve it we want to return just one company.
         """
-        client = Client()
         test_company = Company.objects.create(name="Amazon")
-        companies_url = reverse("companies-list")
-        response = client.get(companies_url)
+        response = self.client.get(self.companies_url)
         print(response.content)
         response_content = json.loads(response.content)[0]
         self.assertEqual(response.status_code, 200)
@@ -38,4 +42,3 @@ class TestGetCompanies(TestCase):
         self.assertEqual(response_content.get("application_link"), "")
         self.assertEqual(response_content.get("notes"), "")
         test_company.delete()
-
