eea7439ec22c9fd4522fd4e81da208f420a20ecd
pavlo <pavlo_ivanov@yahoo.com>
2022-12-15 14:34:23 +0100

    Step 33 - Adding an email sending service. Configured endpoint (send-email) and email sending mechanism. Added EMAIL_HOST_PASSWORD as secret and invironment variable in GitHub and Bitbucket. Edited github-actions.yml to reflect addition of new environment variable.

--- api/coronavstech/companies/views.py
+++ api/coronavstech/companies/views.py
@@ -1,5 +1,8 @@
+from django.core.mail import send_mail
+from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 
 from api.coronavstech.companies.models import Company
 from api.coronavstech.companies.serializers import CompanySerializer
@@ -9,6 +12,21 @@ class CompanyViewSet(ModelViewSet):
     serializer_class = CompanySerializer  # (1)
     queryset = Company.objects.all().order_by("-last_update")  # (2)
     pagination_class = PageNumberPagination  # (3)
+
+@api_view(http_method_names=["POST"])
+def send_company_email(request):
+    """
+    Function that will send email with request payload.
+    With the help of REST FRAMEWORK decorator `@api_view()`
+    we will treat our function as POST endpoint.
+    sender: pavlo_ivanov@yahoo.com
+    receiver: pavlo_ivanov@yahoo.com
+    """
+    send_mail(subject="My cool subject", message="My cool message", from_email="python.testme@gmail.com", recipient_list=["python.testme@gmail.com"])
+    return Response({"status": "success", "info": "email sent successfully"}, status=200)
+
+
+
 
 
 """
--- api/coronavstech/coronavstech/settings.py
+++ api/coronavstech/coronavstech/settings.py
@@ -114,6 +114,15 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "python.testme@gmail.com"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_PASSWORD  = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
