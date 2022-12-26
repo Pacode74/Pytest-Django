6fe032060ff88f42a7a033f529819c407f1238bf
pavlo <pavlo_ivanov@yahoo.com>
2022-12-26 15:53:11 +0100

    Step 41 - Using double parameteriazed to first, test different functions and sencond test those functions with the same arguments.

---
+++ fibonacci/tests/test_different_functions_at_the_same_time_using_parameterized.py
@@ -0,0 +1,46 @@
+from apps.fib_cached import fibonacci_cached, fibonacci_lru_cached
+from apps.fib_recursive import fibonacci_naive
+import pytest
+from typing import Callable
+
+def test_naive_simple() -> None:
+    """Naive testing without parameterized. Without cached."""
+    res = fibonacci_naive(n=0)
+    assert res == 0
+
+    res = fibonacci_naive(n=1)
+    assert res == 1
+
+    res = fibonacci_naive(n=2)
+    assert res == 1
+
+    res = fibonacci_naive(n=20)
+    assert res == 6765
+
+@pytest.mark.parametrize("n, expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
+def test_naive(n: int, expected: int) -> None:
+    """Simply testing naive fibonacci without cached using parameterized"""
+    res = fibonacci_naive(n=n)
+    assert res == expected
+
+@pytest.mark.parametrize("n, expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
+def test_fibonacci_cached(n:int, expected:int)-> None:
+    """Simply testing fibonacci cached using parameterized"""
+    res = fibonacci_cached(n=n)
+    assert res == expected
+
+@pytest.mark.parametrize("n, expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
+def test_fibonacci_lru_cached(n:int, expected:int)-> None:
+    """Simply testing fibonacci_lru_cached using parameterized"""
+    res = fibonacci_lru_cached(n=n)
+    assert res == expected
+
+@pytest.mark.parametrize("fib_func", [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached])
+@pytest.mark.parametrize("n, expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
+def test_fibonacci(fib_func: Callable[[int], int], n: int, expected: int)-> None:
+    """To avoid duplicating above test codes of test_naive, test_fibonacci_cached and
+    test_fibonacci_lru_cached we can test three functions at the same time with
+    the same arguments.You can switch above order of @pytest.mark.parameterized then
+    the visual result will be different."""
+    res = fib_func(n)
+    assert res == expected
--- fibonacci/tests/test_fib_recursive_my_parameterized.py
+++ fibonacci/tests/test_fib_recursive_my_parameterized.py
@@ -1,6 +1,6 @@
 # from apps.fib_recursive import fibonacci_naive
 # from my_parameterized_decodator import my_parametrized
-
+#
 # @my_parametrized(identifiers="n, expected", values=[(0, 0), (1, 1), (2, 1), (20, 6765)])
 # def test_naive(n: int, expected: int) -> None:
 #     res = fibonacci_naive(n=n)
