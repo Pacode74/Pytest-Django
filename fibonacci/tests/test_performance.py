1edeaa2de4ca18d52ed16b06c1932b40684fc1bb
pavlo <pavlo_ivanov@yahoo.com>
2022-12-28 16:36:34 +0100

    Step 45 - Implement a pytest performance validator. Build custom pytest-timeout decorator. Use `pytest.ini`. Step 46 - plugin, pytest-timeout decorator.

---
+++ fibonacci/tests/test_performance.py
@@ -0,0 +1,17 @@
+import pytest
+from time import sleep
+from fibonacci.apps.fib_dynamic import fibonacci_dynamic_v2
+## Option 1: Result is the same as option 2. I separated decorator and fixture:
+# from fibonacci.apps.track_performance_decorator import track_performance_decorator
+
+## Option 2: Result is the same as option 1. Teacher didn't separate decorator and fixture:
+from conftest import track_performance_decorator
+
+
+@pytest.mark.performance
+@track_performance_decorator
+def test_performance():
+    """1) mark it with performance marker that is registered in pytest.ini.
+    2) decorate it track_performance_decorator"""
+    # sleep(3)
+    fibonacci_dynamic_v2(1000)
