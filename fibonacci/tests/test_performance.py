511318bd41d459a61b6822c4086ed0b02b85e6bd
pavlo <pavlo_ivanov@yahoo.com>
2022-12-28 23:20:02 +0100

    Testing Slack and testing that PerformanceException is not raisen in fibonacci/tests/test_performance.py.

--- fibonacci/tests/test_performance.py
+++ fibonacci/tests/test_performance.py
@@ -13,5 +13,5 @@ from conftest import track_performance_decorator
 def test_performance():
     """1) mark it with performance marker that is registered in pytest.ini.
     2) decorate it track_performance_decorator"""
-    sleep(3)
+    # sleep(3)
     fibonacci_dynamic_v2(1000)
